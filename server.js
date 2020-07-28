cfe9025409b2771d4b270f251c24d9c0350bd68c
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2020-07-28 17:50:00 -0400

    updating database

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',   //local host
+      host : 'postgresql-trapezoidal-72842',
       user : 'postgres',
       password : 'Faylinshaw00',
       database : 'smartbrain'
