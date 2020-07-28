b7804cc0fd6316ec3a12dfc54e6bdc5d527e6733
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2020-07-28 17:54:53 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-trapezoidal-72842',
-      user : 'postgres',
-      password : 'Faylinshaw00',
-      database : 'smartbrain'
+      host : process.env.DATABASE_URL,
+      ssl : true
     }
 });
 
