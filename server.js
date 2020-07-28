75e7af37007fcd57fc327a272442f8f6e150d718
Blaire Swaby-Clacken <blaireswabyclacken@yahoo.com>
2020-07-28 15:38:20 -0400

    adding port

--- server.js
+++ server.js
@@ -33,8 +33,8 @@ app.put('/image', image.handleImage(db))
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(3000, ()=> {
-    console.log('App is running on port 3000');
+app.listen(process.env.PORT || 3000, ()=> {
+    console.log(`App is running on port 3000 ${process.env.PORT}`);
 })
 
 
